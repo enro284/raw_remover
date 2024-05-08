62a1bd472f6d53d83291f92d87187ff8eeca83dc
Enrico Barbuio <148079534+enro284@users.noreply.github.com>
2024-05-08 23:33:18 +0200

    Add main.cpp: proof of concept, barely functional

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[]) {
+  std::cout
+      << "Welcome to RAW REMOVER\n"
+      << "this program checks every image in the folder where it's executed\n"
+      << "and deletes the raw image if it doesn't find the matching compressed "
+         "image\n\n";
+
+  std::string comp_ext;
+  std::string raw_ext;
+  std::cout << "insert compressed image extension (including \".\"): ";
+  std::cin >> comp_ext;
+  std::cout << "insert raw image extension (including \".\"): ";
+  std::cin >> raw_ext;
+
+  for (auto& dir_entry : fs::directory_iterator(".")) {
+    if (dir_entry.is_regular_file()) {
+      auto file = dir_entry.path();
+      if (file.extension().string() == raw_ext &&
+          !exists(file.replace_extension(comp_ext))) {
+        std::cout << "deleting: " << dir_entry << '\n';
+        fs::remove(dir_entry);
+      }
+    }
+  }
+}
